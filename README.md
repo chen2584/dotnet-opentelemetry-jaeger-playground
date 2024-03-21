f086b7c6b3cb2fd1b72abe95966ca18b24807fc2
Worameth Semapat <worameth@ookbee.com>
2024-03-22 02:20:50 +0700

    feat: otel collector oidc with keycloak

---
+++ README.md
@@ -0,0 +1,17 @@
+#  aspnet-opentelemetry-playground
+
+### Generate TLS files (Self-Certificate) for Test purpose (Not Required)
+```
+cfssl genkey -initca ca-csr.json | cfssljson -bare ca
+cfssl gencert -ca ca.pem -ca-key ca-key.pem cert-csr.json | cfssljson -bare client
+cfssl gencert -ca ca.pem -ca-key ca-key.pem cert-csr.json | cfssljson -bare server
+```
+
+### Keyclock Generate JWT Access Token Example
+```
+curl --location --request POST 'http://keycloak:8080/realms/opentelemetry/protocol/openid-connect/token' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'grant_type=client_credentials' \
+--data-urlencode 'client_id=collector' \
+--data-urlencode 'client_secret=ThisIsABook'
+```
--- src/MyApi/Program.cs
+++ src/MyApi/Program.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Net.Http.Headers;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
@@ -29,18 +30,25 @@ builder.Services.AddOpenTelemetry()
         // .AddSource(serviceName)
         .SetErrorStatusOnException(false)
         .AddAspNetCoreInstrumentation()
-        .AddHttpClientInstrumentation()
+        // .AddHttpClientInstrumentation()
         .AddOtlpExporter(o =>
         {
-            o.Endpoint = new Uri("http://localhost:4317"); // Default ไม่ต้องใส่ก็ได้
-        }))
-    .WithMetrics(metrics => metrics
-        .AddAspNetCoreInstrumentation()
-        .AddHttpClientInstrumentation()
-        .AddOtlpExporter(o =>
-        {
-            o.Endpoint = new Uri("http://localhost:4317"); // Default ไม่ต้องใส่ก็ได้
+            o.Endpoint = new Uri("http://localhost:4318/v1/traces"); // Default ไม่ต้องใส่ก็ได้,
+            o.Protocol = OtlpExportProtocol.HttpProtobuf;
+            o.HttpClientFactory = () =>
+            {
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJHVXlFWVVBVTZ4S1RzdW5VY3BCRjlpUW5jc240UVYyUVI1M2VjUUNTeUZzIn0.eyJleHAiOjE3MTEwODMzODcsImlhdCI6MTcxMTA0NzM4NywianRpIjoiOGVlNjA1MTgtNGIyZS00MmEwLTlmYTUtMjc3NWI4MjkyMDEwIiwiaXNzIjoiaHR0cDovL2tleWNsb2FrOjgwODAvcmVhbG1zL29wZW50ZWxlbWV0cnkiLCJhdWQiOlsiY29sbGVjdG9yIiwiYWNjb3VudCJdLCJzdWIiOiI5MWY0OWNiNy1iMWMyLTRkZjEtYWNjOC0yMjYyMzFkOTI5ZjYiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJjb2xsZWN0b3IiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIi8qIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIiwiZGVmYXVsdC1yb2xlcy1vcGVudGVsZW1ldHJ5Il19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiY2xpZW50SG9zdCI6IjE3Mi4yNy4wLjEiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6InNlcnZpY2UtYWNjb3VudC1jb2xsZWN0b3IiLCJjbGllbnRBZGRyZXNzIjoiMTcyLjI3LjAuMSIsImNsaWVudF9pZCI6ImNvbGxlY3RvciJ9.3vHUizpLQVbkCJ57C_J0_QaIWn4s070auWoYbeN42itl38zf0ivwSqfMveVonlQI5EOkvwp_tc6I4FqVhdKp1a0f8KKr3zlR71OcyVrsmzisCSjvZf7Y0nhPIMuuHTM9mmdzZz5tZG0jNK2Ni_8O4CdO37qoxoiXZJtIosSEpkAsE_i-mAFTnNxtPgjaf0HYJVimHdcvrFfBcwKIZaT0hl-AFXieMLg7aTnoMn5e0E0x_Wndjm1X1bue6FSpEgGyx4GO0BMwlnHIgpo9UIyM5fDMdwmm7cQ6nR69yLlkgP7U1sX1FBEm_Ra1LImpqJbclmC5LqmV-ZjTUChsoi6uTw");
+                return client;
+            };
         }));
+    // .WithMetrics(metrics => metrics
+    //     .AddAspNetCoreInstrumentation()
+    //     .AddHttpClientInstrumentation()
+    //     .AddOtlpExporter(o =>
+    //     {
+    //         o.Endpoint = new Uri("http://localhost:4317"); // Default ไม่ต้องใส่ก็ได้
+    //     }));
 
 builder.Services.AddSingleton(_ => new ActivitySource(serviceName));
 
